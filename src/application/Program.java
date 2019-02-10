b22f595e3826b46339670c2c554a9b16a36eeced
Egon Patrick <egon.16@hotmail.com>
2019-02-10 18:58:34 -0200

    Implemetacao delete

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -12,8 +13,9 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		SellerDao sellerDao = DaoFactory.createSellerDao();		
+		Scanner sc = new Scanner(System.in);
 		
+		SellerDao sellerDao = DaoFactory.createSellerDao();				
 		
 		System.out.println("==== TEST 1: seller findById ====");
 		Seller seller = sellerDao.findById(3);
@@ -41,6 +43,14 @@ public class Program {
 		seller.setName("Martha Waine");
 		sellerDao.update(seller);
 		System.out.println("Update completed!");
+		
+		System.out.println("\n==== TEST 6: seller delete ====");		
+		System.out.print("Enter id for delete teste: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete id "+ id + " completed!");
+		
+		sc.close();
 ;	}
 
 }
